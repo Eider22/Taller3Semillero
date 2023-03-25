f9a595a98ad24f763ea4643c417df1dcda99f3fa
Eider22 <eidercardona22@gmail.com>
2023-03-24 19:36:53 -0500

    Se intenta de nuevo subir los cambios por errores con el commit anterior

--- src/taller3/reproductor/Main.java
+++ src/taller3/reproductor/Main.java
@@ -9,17 +9,16 @@ public class Main {
 		Reproductor mp3 = new Mp3(195, 150, "Juan Agudelo", "Pop");
 		Reproductor wav = new Wav(180, 175, "Robert Iglesias", "Rock");
 		Reproductor mp4 = new Mp4(205, 190, "Alfredo Castro", "Drama");
-		
-		
+
 		mp3.reproducir();
 		wav.reproducir();
 		mp4.reproducir();
-		
+
 		List<Reproductor> reproductores = new ArrayList<Reproductor>();
 		reproductores.add(mp3);
 		reproductores.add(wav);
 		reproductores.add(mp4);
-		
+
 		for (Reproductor reproductor : reproductores) {
 			reproductor.reproducir();
 		}
